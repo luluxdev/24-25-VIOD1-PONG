fbc64424e96316658258cb3c34e41e6bd911b891
Lucía Puga <lucia.puga@enti.cat>
2025-01-13 13:43:10 +0100

    Movement and updates.

--- Assets/SCRIPTS/Movimiento2D.cs
+++ Assets/SCRIPTS/Movimiento2D.cs
@@ -6,25 +6,30 @@ public class Movimiento2D : MonoBehaviour
 {
 
     public float speed;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    public bool jugadorA;
 
     // Update is called once per frame
     void Update()
     {
-       
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        float horizontal, vertical;
+
+        if (jugadorA)
+        {
+            horizontal = Input.GetAxis("HorizontalA");
+            vertical = Input.GetAxis("VerticalA");
+        }
+        else
+        {
+            horizontal = Input.GetAxis("HorizontalB");
+            vertical = Input.GetAxis("VerticalB");
+        }
 
         Vector3 direccion = new Vector3(horizontal, vertical);
-        Debug.Log(direccion);
+        // Debug.Log(direccion);
 
         transform.position += direccion * speed * Time.deltaTime;
-        Debug.Log(Time.deltaTime);
+        // Debug.Log(Time.deltaTime); -> cuánto tiempo ocupa un fotograma.
+        // ¿Por qué utilizamos el deltaTime? -> para democratizar las ventajas según los componentes.
 
     }
 }
